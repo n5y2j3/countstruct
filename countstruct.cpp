148f67faa5b10f2608c9befaaf464712e7205c70
JESSI <51254572+n5y2j3@users.noreply.github.com>
2019-07-19 16:37:46 +0900

    Update countstruct.cpp

--- countstruct.cpp
+++ countstruct.cpp
@@ -1,60 +1,77 @@
-#include<eosio/eosio.hpp>
+#include <eosio/eosio.hpp>
 
 using namespace eosio;
 
-CONTRACT addressbook: public contract {
+CONTRACT address: public contract {
     public:
-        using contract::contract;
 
-ACTION countaction(name user){
-    require_auth(user);
+    using contract::contract;
 
-    counts forCount(get_self(), get_self().value);
-    auto itr = forCount.find(user.value);
+    ACTION countuser(name user){
+        require_auth(get_self());
 
-    if(itr == forCount.end()) {
-        forCount.emplace(user, [&] (auto& row) {
+        address_index forCountuser(get_self(), get_self().value);
+        auto itr = forCountuser.require_find(user.value, "NO USER, INSERT IT");
+        if(itr != forCountuser.end()){
+            forCountuser.modify(itr, user, [&](auto& row){
+                row.user = user;
+            row.count ++;
+            });
+        }
+        else {
+            forCountuser.emplace(user, [&](auto& row){
+                row.user = user;
+                row.count ++;
+            });
+        }
+        print(itr -> count);
+    }
+    
+    ACTION findcount(uint64_t count){
+        address_index forFindcount(get_self(), get_self().value);
+        auto forSecondary = forFindcount.get_index<"bycount"_n>();
+
+        auto itr = forFindcount.require_find(count, "NO");
+
+        print (itr->user);
+    }
+
+    ACTION insert(name user){
+        require_auth(get_self());
+
+
+        address_index forInsert(get_self(), get_self().value);
+        auto itr = forInsert.find(user.value);
+
+        check(itr == forInsert.end(), "already exists");
+
+        forInsert.emplace(user, [&](auto& row){
             row.user = user;
-            row.count = 1;
         });
-    } else {
-        forCount.modify(itr, user, [&] (auto& row) {
-            row.count++;
-        });
+
+        print("!!!INSERT SUCCESS!!!");
     }
 
-    print("success");
-};
+    ACTION eraseall() {
+        require_auth(get_self());
 
-ACTION findbycount(uint64_t count) {
-    counts forFind(get_self(), get_self().uservalue);
-    auto forSecondary = forfind.get_index<"bycount"_n>();
-    auto itr = forSecondary.find(count);
+        address_index forEraseAll(get_self(), get_self().value);
+           auto itr = forEraseAll.begin();
+           while(itr != forEraseAll.end()) { itr = forEraseAll.erase(itr); }
+           itr ++;
 
-    if(itr != forSecondary.end()) {
-        print(itr->user, " ", itr->count);
-    } else {
-        print("nobody has that count number");
+           print("!!!ERASE ALL!!!");
     }
-}
 
-ACTION eraseall() {
-    require_auth(get_self());
-
-    address_index forEraseAll(get_self(), get_self().value);
-    auto itr = forEraseAll.begin();
-    while(itr != forEraseAll.end()) { itr = forEraseAll.erase(itr); }
-}
-
-private:
-    TABLE countstruct {}
+    private:
+    struct [[eosio::table]] person {
         name user;
-        uint32_t count;
+        uint64_t count;
 
         uint64_t primary_key() const { return user.value; }
         uint64_t by_count() const { return count; }
-};
+    };
 
-typedef multi_index<"counttable"_n, person,
-indexed_by<"bycount"_n, const_mem_fun<person, uint64_t, &countstruct::by_count>>> counts;
+    typedef multi_index<"counttable"_n, person,
+    indexed_by<"bycount"_n, const_mem_fun<person, uint64_t, &person::by_count>>> address_index;
 };
